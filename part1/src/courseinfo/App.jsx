f1ed593865d66bb1d7eef9da3379a449fcef7859
umidrza <umidrza4@gmail.com>
2026-03-13 12:31:01 +0400

    course info 1.5

--- part1/src/courseinfo/App.jsx
+++ part1/src/courseinfo/App.jsx
@@ -34,30 +34,32 @@ const Total = ({parts}) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
 
   return (
     <div>
-      <Header course={course}/>
+      <Header course={course.name}/>
 
-      <Content parts={parts}/>
+      <Content parts={course.parts}/>
 
-      <Total parts={parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
