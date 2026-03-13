f6795c8ea26dc4e52a9d544817de954cb66842f3
umidrza <umidrza4@gmail.com>
2026-03-13 20:34:34 +0400

    anecdotes

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import App from './unicafe/App.jsx'
+import App from './anecdotes/App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
