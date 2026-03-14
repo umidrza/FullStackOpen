6b19d3279aa3f128514936d9ee28cc76cf399cfc
umidrza <umidrza4@gmail.com>
2026-03-14 22:16:45 +0400

    countries

--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './phonebook/App'
+import App from './countries/App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(<App />)
