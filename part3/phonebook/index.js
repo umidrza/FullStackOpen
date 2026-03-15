11473d7cd3a26ba5c44fb1cf6f99d4487d5191c9
umidrza <umidrza4@gmail.com>
2026-03-15 22:39:05 +0400

    phonebook mongodb endpoints

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-require('dotenv').config()
+const Person = require('./models/person')
 
 const app = express()
 
@@ -12,103 +13,58 @@ app.use(express.static('dist'))
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-app.get('/info', (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        ${new Date()}`);
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
+        .catch(error => {
+            response.status(500).json({ error: 'database error' })
+        })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const note = persons.find((note) => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            response.status(400).json({ error: 'malformatted id' })
+        })
 })
 
-const generateId = () => {
-    const maxId =
-        persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0
-    return String(maxId + 1)
-}
-
 app.post('/api/persons', (request, response) => {
-    const body = request.body;
+    const { name, number } = request.body
+    const person = new Person({ name, number })
 
-    if (!body) {
-        return response.status(400).json({
-            error: 'content missing',
+    person.save()
+        .then(savedPerson => {
+            response.status(201).json(savedPerson)
         })
-    }
-
-    if (!body.name){
-        return response.status(400).json({
-            error: 'name is required ',
+        .catch(error => {
+            response.status(500).json({ error: 'database error' })
         })
-    }
-
-    if (!body.number){
-        return response.status(400).json({
-            error: 'number is required ',
-        })
-    }
-
-    if (persons.some(p => p.name == body.name)){
-        return response.status(400).json({
-            error: 'name must be unique' ,
-        })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId(),
-    }
-
-    persons = persons.concat(person)
-
-    response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter((note) => note.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            response.status(400).json({ error: 'malformatted id' })
+        })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -18,7 +18,16 @@ const personSchema = new mongoose.Schema({
     number: String,
 })
 
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 const Person = mongoose.model('Person', personSchema)
+
 
 if (process.argv.length == 5) {
     const person = new Person({
