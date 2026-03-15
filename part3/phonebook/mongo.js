ff02e9f1038e2c92572927433ae59e970e118106
umidrza <umidrza4@gmail.com>
2026-03-15 21:42:22 +0400

    phonebook mongodb

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,31 +1,41 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
+    console.log('give password as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://umidrza_db_user:${password}@cluster0.jvq0ebc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://umidrza_db_user:${password}@cluster0.jvq0ebc.mongodb.net/phonebook?appName=Cluster0`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url, { family: 4 })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+    name: String,
+    number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-  name: 'Dwight Scrute',
-  number: '444-22-345434',
-})
+if (process.argv.length == 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
 
-person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+    person.save().then(result => {
+        console.log(result)
+        mongoose.connection.close()
+    })
+}
+else {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
