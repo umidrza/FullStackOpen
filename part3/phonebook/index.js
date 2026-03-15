45cbfaca341b47911488fb66e52b313bcfa82906
umidrza <umidrza4@gmail.com>
2026-03-15 17:54:20 +0400

    build

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -104,7 +108,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
